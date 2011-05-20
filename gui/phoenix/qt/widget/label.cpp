b9658d69f9d10901430c118959bec7b87a958d23
Themaister <maister@archlinux.us>
2011-05-21 01:22:06 +0200

    Don't need GUI just yet ...

--- gui/phoenix/qt/widget/label.cpp
+++
@@ -1,7 +0,0 @@
-void pLabel::setText(const string &text) {
-  qtLabel->setText(QString::fromUtf8(text));
-}
-
-void pLabel::constructor() {
-  qtWidget = qtLabel = new QLabel;
-}
--- gui/phoenix/qt/widget/progress-bar.cpp
+++
@@ -1,9 +0,0 @@
-void pProgressBar::setPosition(unsigned position) {
-  qtProgressBar->setValue(position);
-}
-
-void pProgressBar::constructor() {
-  qtWidget = qtProgressBar = new QProgressBar;
-  qtProgressBar->setRange(0, 100);
-  qtProgressBar->setTextVisible(false);
-}
